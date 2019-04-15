082e491ebcbd7eb53ffc41f3720628cc890f79c1
Andrew <paladzin@gmail.com>
2019-04-15 18:19:25 +0300

    Update delayer_settings.py

--- delayer_settings.py
+++ delayer_settings.py
@@ -1,5 +1,5 @@
 STREAM_DIR = '/tmp/nginx'
-STREAM_DESTINATION = 'rtmp://127.0.0.1:1337/live'
-DELAY = 120     #Seconds
+STREAM_DESTINATION = 'rtmp://127.0.0.1:1935/live'
+DELAY = 60     #Seconds
 BACKUPSTREAM_SHORT = '' #Show while intermission
 BACKUPSTREAM_LONG = ''  #Show while longer downtime
